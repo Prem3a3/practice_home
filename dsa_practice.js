ecc9870e96a195bf6f95090404823017400423ea
chandu@example.com <chandum>
2023-10-14 21:36:53 +0530

    bubble sort

---
+++ dsa_practice.js
@@ -0,0 +1,13 @@
+//DSA  practice from youtube
+var array1 = [20, 34, 89, 56];
+/**
+ * bubble sort with time complexity O(n2)+1
+ */
+for (var i = 0; i < array1.length; i++) {
+    for (var j = 0; j < array1.length; j++) {
+        if (array1[j] > array1[j + 1]) {
+            array1[j] = array1[j + 1] + array1[j] - (array1[j + 1] = array1[j]);
+        }
+    }
+}
+console.warn(array1);
--- dsa_practice.ts
+++ dsa_practice.ts
@@ -1,1 +1,17 @@
 //DSA  practice from youtube
+
+const array1 = [20, 34, 89, 56];
+/**
+ * bubble sort with time complexity O(n2)+1
+ */
+for (let i = 0; i < array1.length; i++) {
+    for (let j = 0; j < array1.length; j++) {
+            if(array1[j] > array1[j+1])
+            {
+                array1[j] = array1[j+1]+array1[j] - (array1[j+1] =array1[j] )
+            }
+
+    }
+}
+console.warn(array1)
+
