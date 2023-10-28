56d0bbeba5ec249d0bbfb9968671b835f532448a
chandu@example.com <chandum>
2023-10-28 19:37:36 +0530

    completed changes

--- dsa_practice.js
+++ dsa_practice.js
@@ -2,6 +2,7 @@
 var array1 = [20, 34, 89, 56];
 /**
  * bubble sort with time complexity O(n2)+1
+ * worst case is O(1)
  */
 for (var i = 0; i < array1.length; i++) {
     for (var j = 0; j < array1.length; j++) {
--- dsa_practice.ts
+++ dsa_practice.ts
@@ -3,15 +3,17 @@
 const array1 = [20, 34, 89, 56];
 /**
  * bubble sort with time complexity O(n2)+1
+ * worst case is O(1)
  */
 for (let i = 0; i < array1.length; i++) {
     for (let j = 0; j < array1.length; j++) {
-            if(array1[j] > array1[j+1])
-            {
-                array1[j] = array1[j+1]+array1[j] - (array1[j+1] =array1[j] )
-            }
+        if (array1[j] > array1[j + 1]) {
+            array1[j] = array1[j + 1] + array1[j] - (array1[j + 1] = array1[j])
+        }
 
     }
 }
-console.warn(array1)
+
+
+console.warn(array1);
 
